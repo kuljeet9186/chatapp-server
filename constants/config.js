1a931a066726ba7f157bc5d77314c8a5c67b1945
kuljeet singh <kuljeet9186@gmail.com>
2024-06-20 19:49:27 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://chat-sphere-three.vercel.app/",
+    "https://chat-sphere-k82hq4o1j-kuljeets-projects.vercel.app/",
     "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
