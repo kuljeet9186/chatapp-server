6a8bfc2514e3a041229f2a7af29d7d170a736247
kuljeet singh <kuljeet9186@gmail.com>
2024-06-20 20:32:55 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,17 @@
 const corsOptions = {
-  origin: '*',
+  origin: [
+    'https://chat-sphere-k82hq4o1j-kuljeets-projects.vercel.app',
+    "http://localhost:5173",
+    "https://chatapp-server-1gne.onrender.com",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
 const CHATTU_TOKEN = "chattu-token";
 
 export { corsOptions, CHATTU_TOKEN };
+
